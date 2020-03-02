c73ed47e272dccb7704c2f5685227cb9184a16fd
shawnwardgithub <weidong682011@163.com>
2020-03-02 11:38:32 +0800

    提交了新的东西

---
+++ dataSource/src/main/java/ListDemo/Node.java
@@ -0,0 +1,37 @@
+package ListDemo;
+
+
+/**
+ * @author weidongzhengxin
+ * @date 2/29/2020 6:46 PM
+ */
+public class Node {
+    private final Integer value;
+    private Node next;
+
+    public Node(Integer value){
+        this.value = value;
+        this.next = null;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Node{");
+        sb.append("value=").append(value);
+        sb.append(", next=").append(next);
+        sb.append('}');
+        return sb.toString();
+    }
+}
---
+++ dataSource/src/main/java/ListDemo/listTestClass.java
@@ -0,0 +1,51 @@
+package ListDemo;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author weidongzhengxin
+ * @date 2/29/2020 6:47 PM
+ */
+public class listTestClass {
+
+    //创建正向链表的方法
+    public Node creatCommonListWithHead(List<Integer> src){
+        if(src.isEmpty()){
+            return null;
+        }
+        Node firstNode = new Node(src.get(0));
+        Node header = creatCommonListWithHead(src.subList(1,src.size()));
+        firstNode.setNext(header);
+        return firstNode;
+    }
+
+    //链表反转的方法
+    public Node reverCommonListBeforeHeader(Node node){
+        if(node==null){
+            return null;
+        }
+        if(node.getNext()==null){
+            return node;
+        }
+        Node headNode = reverCommonListBeforeHeader(node.getNext());
+        node.getNext().setNext(node);
+        node.setNext(null);
+        return headNode;
+    }
+
+
+
+
+
+
+    public static void main(String[] args) {
+        listTestClass listTestClass = new listTestClass();
+        Node node = listTestClass.creatCommonListWithHead(Arrays.asList(1,2,3,4,5));
+        System.out.println(node);
+        //反转列表
+        System.out.println(listTestClass.reverCommonListBeforeHeader(node));
+    }
+
+
+}
