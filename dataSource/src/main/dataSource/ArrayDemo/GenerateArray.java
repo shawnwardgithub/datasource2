1038ff8cbefe43bcf5393f02e07f348a4840fc01
shawnwardgithub <weidong682011@163.com>
2020-03-05 09:36:42 +0800

    2020 03 05 上传数据

--- dataSource/src/main/dataSource/ArrayDemo/GenerateArray.java
+++ dataSource/src/main/dataSource/ArrayDemo/GenerateArray.java
@@ -27,7 +27,7 @@ public class GenerateArray {
         return true;
     }
     //查询是否存在该数据
-    public boolean getCurrentValue(int value){
+    public int getCurrentValue(int value){
         int i;
         for(i=0;i<nsize;i++){
            if(values[i]==value){
@@ -35,9 +35,9 @@ public class GenerateArray {
            }
         }
         if(i==nsize){
-            return false;
+            return -1;
         }else{
-            return true;
+            return i;
         }
     }
     //删除某个数据
@@ -52,23 +52,22 @@ public class GenerateArray {
         if(i==nsize){
             return false;
         }
-        for(int k = i;k<nsize-1;k++){
-           this.values[k] = this.values[k+1];
+        if(nsize == maxSize){
+            for(int k = i;k<nsize-1;k++){
+                this.values[k] = this.values[k+1];
+            }
+            values[nsize-1] = 0;
+        }else{
+            for(int k = i;k<nsize;k++){
+                this.values[k] = this.values[k+1];
+            }
         }
-//        if(nsize == maxSize){
-//            for(int k = i;k<maxSize-1;k++){
-//                this.values[k] = this.values[k+1];
-//            }
-//        }else{
-//            for(int k = i;k<nsize;k++){
-//                this.values[k] = this.values[k+1];
-//            }
-//        }
         nsize--;
         return true;
     }
 
     public void display(){
+        System.out.println("-------------------");
         for (int value : this.values) {
             System.out.println(value);
         }
@@ -78,6 +77,14 @@ public class GenerateArray {
         GenerateArray generateArray = new GenerateArray(5);
         //generateArray.display();
         generateArray.insertValue(3);
+        generateArray.insertValue(2);
+        generateArray.insertValue(4);
+        generateArray.display();
+        System.out.println(generateArray.getCurrentValue(3));
+        System.out.println(generateArray.getCurrentValue(2));
+
+        //删除数据
+        generateArray.deleteCurrentValue(2);
         generateArray.display();
     }
 }
